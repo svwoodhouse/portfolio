94c66e6ca1c73e1a33e20a0aaab7d900840445cd
Sydnee Woodhouse <svwoodhouse@gmail.com>
2024-01-15 12:38:44 -0500

    Fixed download resume issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar() {
         </a>
         <a
           href={resume}
-          download={resume}
+          download='SYDNEE_WOODHOUSE_RESUME'
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
           Download Resume
         </a>
